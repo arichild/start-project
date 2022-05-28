cee2b028eb3db7019b140197ac4e81e11ee01fd2
arichild <arieevq@gmail.com>
2022-05-28 22:22:21 +0300

    in svg remove fill\style

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,7 +1,17 @@
 import svgSprite from 'gulp-svg-sprite'
+import cheerio from 'gulp-cheerio'
+import replace from 'gulp-replace'
 
 export const svg = () => {
 	return app.gulp.src(app.path.src.svg)
+    .pipe(cheerio({
+      run: function ($) {
+        $('[fill]').removeAttr('fill');
+        $('[style]').removeAttr('style');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(replace('&gt;', '>'))
     .pipe(svgSprite({
       mode: {
         stack: {
